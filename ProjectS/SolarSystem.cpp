90dfe414efdee5eb8e83e1f8722a4fe735b648d4
Sahil Khatri <sahilxhettri633@gmail.com>
2026-01-26 19:13:34 +0545

    New

---
+++ ProjectS/SolarSystem.cpp
@@ -0,0 +1,16 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+using namespace std;
+
+// ---------------- Window ----------------
+int WIDTH = 1024, HEIGHT = 768;
+
+
+int main() {
+   
+    return 0;
+}
