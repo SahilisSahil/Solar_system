09ec5aa947ea410264d589b3259a4f32b70a8739
Sahil Khatri <sahilxhettri633@gmail.com>
2026-01-28 12:14:48 +0545

    structure

--- ProjectS/SolarSystem.cpp
+++ ProjectS/SolarSystem.cpp
@@ -8,7 +8,17 @@ using namespace std;
 
 // ---------------- Window ----------------
 int WIDTH = 1024, HEIGHT = 768;
+float camDistance = 700.0f;
+float camYaw = 20.0f, camPitch = 28.0f;
+float camTargetX = 0.0f, camTargetY = 0.0f, camTargetZ = 0.0f;
 
+struct Planet {
+    string name;
+    float radius;
+    float distance;
+    float orbitSpeed;
+    float angle;
+    }
 
 int main() {
    
