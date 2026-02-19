32083acf29244d2e1b1ec1b2da3ea707a7a7f54b
Sahil chhettri <130327896+SahilisSahil@users.noreply.github.com>
2026-02-19 21:56:19 +0545

    Remove empty lines in SolarSystem.cpp
    smallest

--- ProjectS/SolarSystem.cpp
+++ ProjectS/SolarSystem.cpp
@@ -1,11 +1,8 @@
-
-
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>  
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 #include <iostream>
 #include <vector>
 #include <string>
